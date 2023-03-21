d799ce86025cf3b002aeccb050224d3610be7a68
Raffaele-fs <118808372+Raffaele-fs@users.noreply.github.com>
2023-03-21 09:02:45 +0100

    Add files via upload

---
+++ _1/Course.java
@@ -0,0 +1,11 @@
+package javaoop.exercises._1;
+
+public class Course {
+    String courseName ="java";
+    double maxStudents=50;
+    int qualityRatingOutOf10=9;
+
+    double timeCourse=125;
+    String commentCourse="";
+
+}
